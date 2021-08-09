6f86b5328a48c033cb49c84a175f06f0743c149d
jamesyoung-info <me@jamesyoung.info>
2021-08-09 14:56:55 +1000

    Add name to User table

--- database/migrations/1628479313851_users.ts
+++ database/migrations/1628479313851_users.ts
@@ -6,6 +6,7 @@ export default class UsersSchema extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
+      table.string('name', 255).nullable()
       table.string('email', 255).notNullable()
       table.string('password', 255).notNullable()
       table.string('remember_me_token').nullable()
